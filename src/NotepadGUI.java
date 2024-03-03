402bab64256563fddcf861cb06fb903b3e81a777
SnowDhrop <nath.arditti@yahoo.fr>
2024-03-03 22:43:22 +0100

    feat: save as, feat: open

--- src/NotepadGUI.java
+++ src/NotepadGUI.java
@@ -1,14 +1,26 @@
 package src;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class NotepadGUI extends JFrame {
+    // File explorer
+    private JFileChooser jFileChooser;
+    private JTextArea textArea;
+
     public NotepadGUI() {
         super("Notepad");
         setSize(400, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        jFileChooser = new JFileChooser();
+        jFileChooser.setCurrentDirectory(new File("src/assets"));
+        jFileChooser.setFileFilter(new FileNameExtensionFilter("Text Files", "txt"));
 
         addGuiComponents();
     }
@@ -31,7 +43,7 @@ public class NotepadGUI extends JFrame {
 
         add(toolBar, BorderLayout.NORTH);
 
-        JTextArea textArea = new JTextArea();
+        textArea = new JTextArea();
         add(textArea, BorderLayout.CENTER);
     }
 
@@ -40,14 +52,89 @@ public class NotepadGUI extends JFrame {
 
         // New
         JMenuItem newMenuItem = new JMenuItem("New");
+        newMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Reset Notepad
+                setTitle("Notepad");
+                textArea.setText("");
+            }
+        });
         fileMenu.add(newMenuItem);
 
         // Open
         JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = jFileChooser.showOpenDialog(NotepadGUI.this);
+                if (result != JFileChooser.APPROVE_OPTION) return;
+
+                try {
+                    // Reset notepad
+                    newMenuItem.doClick();
+
+                    File selectedFile = jFileChooser.getSelectedFile();
+                    setTitle(selectedFile.getName());
+
+                    // Read the file
+                    FileReader fileReader = new FileReader(selectedFile);
+                    BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+                    // Store the text
+                    StringBuilder fileText = new StringBuilder();
+                    String readText;
+                    while ((readText = bufferedReader.readLine()) != null) {
+                        fileText.append(readText + "\n");
+                    }
+
+                    // Update text area gui
+                    textArea.setText(fileText.toString());
+
+                } catch(Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
         fileMenu.add(openMenuItem);
 
         // Save as
         JMenuItem saveAsMenuItem = new JMenuItem("Save as");
+        saveAsMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int result = jFileChooser.showSaveDialog(NotepadGUI.this);
+                if (result != JFileChooser.APPROVE_OPTION) return;
+
+                try {
+                    File selectedFile = jFileChooser.getSelectedFile();
+
+                    // Append .txt to the file
+                    String fileName = selectedFile.getName();
+                    if (!fileName.substring(fileName.length() - 4).equalsIgnoreCase(".txt")) {
+                        System.out.println("HZEERE");
+                        selectedFile = new File(selectedFile.getAbsoluteFile() + ".txt");
+                    }
+
+                    selectedFile.createNewFile();
+
+                    // Write the user's text into the file created
+                    FileWriter fileWriter = new FileWriter(selectedFile);
+                    BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                    bufferedWriter.write(textArea.getText());
+
+                    bufferedWriter.close();
+                    fileWriter.close();
+
+                    // Update the title header of the gui to the save text file
+                    setTitle(fileName);
+
+                    JOptionPane.showMessageDialog(NotepadGUI.this, "File saved !");
+                } catch(Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+        });
         fileMenu.add(saveAsMenuItem);
 
         // Save
